786e8a7ac6920bfdcf3573e68a796893697cb2d9
OPOKU-NUAMAH <francisopoku998@gmail.com>
2024-03-29 19:46:04 +0000

    all

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""changes the name of a State object from
-the database hbtn_0e_6_usa"""
+"""prints all City objects
+from the database hbtn_0e_14_usa"""
 
 if __name__ == "__main__":
 
     import sys
     from model_state import Base, State
+    from model_city import City
     from sqlalchemy import create_engine
     from sqlalchemy.orm import Session
     from sqlalchemy.schema import Table
@@ -16,7 +17,8 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
 
     session = Session(engine)
-    state = session.query(State).filter(State.id == 2).first()
-    state.name = 'New Mexico'
-    session.commit()
+    for state, city in session.query(State, City)\
+                              .filter(City.state_id == State.id)\
+                              .order_by(City.id).all():
+            print("{}: ({}) {}".format(state.name, city.id, city.name))
     session.close()
