13392e6f42e42a6f8589fbf3fc6911aaab9bfe80
OPOKU-NUAMAH <francisopoku998@gmail.com>
2024-03-29 15:12:58 +0000

    all

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """takes in an argument and displays all values
 in the states table of hbtn_0e_0_usa
-where name matches the argument
-and is safe from SQL injections"""
+where name matches the argument"""
 
 if __name__ == '__main__':
 
@@ -13,8 +12,8 @@ if __name__ == '__main__':
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name=%s\
-                ORDER BY states.id ASC", (sys.argv[4],))
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
+                ORDER BY states.id ASC".format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
